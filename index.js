02e2849da766fc3b1980af249769bfd94af53dc5
fojletuhin <fojletuhin@gmail.com>
2024-05-13 03:56:58 +0600

    post wishlist

--- index.js
+++ index.js
@@ -17,92 +17,98 @@ console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
 
 
-    const blogsCollection = client.db('On-the-go').collection('blogs');
-    const commentCollection = client.db('On-the-go').collection('comments');
+        const blogsCollection = client.db('On-the-go').collection('blogs');
+        const commentCollection = client.db('On-the-go').collection('comments');
+        const wishlistCollection = client.db('On-the-go').collection('wishList');
 
 
-    //get all blogs from database
-    app.get('/blogs', async (req, res)=>{
-        const cursor =await blogsCollection.find().toArray();
-        res.send(cursor);
-    })
+        //get all blogs from database
+        app.get('/blogs', async (req, res) => {
+            const cursor = await blogsCollection.find().toArray();
+            res.send(cursor);
+        })
 
-    //get a single data by id
+        //get a single data by id
 
-    app.get('/blogs/:id', async(req, res)=>{
-        const id = req.params.id
-        const query={_id: new ObjectId(id)}
-        const result= await blogsCollection.findOne(query)
-        res.send(result)
-    })
+        app.get('/blogs/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await blogsCollection.findOne(query)
+            res.send(result)
+        })
 
+        //get comment for a specific blog
 
-    //insert blogs in database
-    app.post('/blog', async(req, res)=>{
-        const newBlog = req.body;
-        console.log(newBlog);
-        const result = await blogsCollection.insertOne(newBlog);
-        res.send(result)
-    })
+        app.get('/comments', async (req, res) => {
+            const result = await commentCollection.find().toArray();
+            res.send(result)
+        })
 
-
-
-    // insert comment in new Collection
-    app.post('/comment', async(req, res)=>{
-        const newComment = req.body;
-        console.log(newComment);
-        const result = await commentCollection.insertOne(newComment);
-        res.send(result)
-    })
+        //insert blogs in database
+        app.post('/blog', async (req, res) => {
+            const newBlog = req.body;
+            console.log(newBlog);
+            const result = await blogsCollection.insertOne(newBlog);
+            res.send(result)
+        })
 
 
-    //get comment for a specific blog
-    app.get('/comments', async(req, res)=>{
-        const result= await commentCollection.find().toArray();
-        res.send(result)
-    })
+        // insert comment in new Collection
+        app.post('/comment', async (req, res) => {
+            const newComment = req.body;
+            console.log(newComment);
+            const result = await commentCollection.insertOne(newComment);
+            res.send(result)
+        })
 
 
-
-
-
-
-
-
-
-
-
+        //insert blog in wishlist
+        app.post('/wishlist', async(req, res)=>{
+            const newWish = req.body;
+            console.log(newWish)
+            const result = await wishlistCollection.insertOne(newWish);
+            res.send(result)
+        })
 
 
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+
+
+
+
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('On the go is running');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`on the go is running on port ${port}`);
 })
