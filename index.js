133472fc9450f5f4cb04da0d12713b666e2b1063
fojletuhin <fojletuhin@gmail.com>
2024-05-14 02:07:43 +0600

    git update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, AggregationCursor } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -40,6 +40,9 @@ async function run() {
             const cursor = await blogsCollection.find().toArray();
             res.send(cursor);
         })
+
+        //get data by word count
+        
 
         //Update a single data
         app.put('/updateBlog/:id', async (req, res) => {
